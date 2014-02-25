edc241083599e4a64c4cd2b14021e393b59e5b99
tombazza <tombazza@gmail.com>
2014-02-25 09:47:25 +0000

    Building application logic

--- js/main.js
+++ js/main.js
@@ -1,20 +1,26 @@
+(function($) {
 var googleApiKey = 'AIzaSyDi6HPUir9lIRMhFAPKg14wRk5UBSVGh78';
-function initialize() {
+function loadMap() {
   var mapOptions = {
     zoom: 8,
     center: new google.maps.LatLng(-34.397, 150.644)
   };
 
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
 
-function loadScript() {
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
-      'callback=initialize&key=' + googleApiKey;
-  document.body.appendChild(script);
+
+function init() {
+	loadMap();
+}
+
+function registerHandlers() {
+	$('#search').submit(function(e) {
+		var query = $('#search input.search').text();
+		
+		return false;
+	});
 }
 
 $(document).ready(loadScript);
+})(jQuery);
